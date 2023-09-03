a1e84be80f52076e82c1b9a6d2f80b31cbace7e2
Ifra Shafique <ifrashafique123@gmail.com>
2023-09-03 23:27:53 +0500

    backend addition

--- src/Components/StudentPanel/StudentProfile.js
+++ src/Components/StudentPanel/StudentProfile.js
@@ -4,43 +4,131 @@ import img3 from "../Images/logoBlack.png";
 import { Link } from 'react-router-dom';
 import Title from "./Title";
 import Menu from "./Menu";
+import axios from "axios";
 
 export default function StudentProfile() {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [infoMessage, setInfoMessage] = useState("");
+    
+    const initialFormState = {
+      Name: "",
+      ContactNo: "",
+      Address: "",
+      Email: "",
+      Department: "",
+      CurrentSemester: "",
+      CGPA: "",
+      Skills: "",
+      
+    }
 
+    const [formData, setFormData] = useState(initialFormState);
+    const [resume, setResume] = useState(null);
+    
     const toggleMenu = () => {
       setMenuOpen(!menuOpen);
     };
+    
+    // const handleSubmit = (event) => {
+    //   event.preventDefault();
+    //   // Process the form data here
 
+    //   const requiredFields = [
+    //     "Name",
+    //     "ContactNo",
+    //     "Address",
+    //     "Email",
+    //     "Department",
+    //     "CurrentSemester",
+    //     "CGPA",
+    //     "Skills",
+    //   ];
+
+    //   const isAnyFieldEmpty = requiredFields.some(
+    //     (fieldName) => !formData[fieldName]
+    //   );
+    
+    //   if (isAnyFieldEmpty) {
+    //     setErrorMessage("No field should be empty");
+    //     return; // Prevent form submission if any field is empty
+    //   }
+
+    //   else if(!/^\d{11}$/.test(formData.ContactNo)){
+    //     setErrorMessage("Contact no should have exactly 11 digits");
+    //     setInfoMessage("");
+    //   }
+
+    //   setErrorMessage("");
+    //   setInfoMessage("");
+
+    //   console.log("Student Form Data:",formData);
+
+    //   axios.post("http://localhost:4000/stuprofile", {formData, resume})
+    //   .then((result) => {
+    //     console.log("Student Profile Registration", result.data);
+    //     setInfoMessage("Successfully Updated");
+    //     setFormData(initialFormState)
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     setErrorMessage("Registration Failed");
+    //   });
+    //   };
+
+    const handleFieldChange = (fieldName, value) => {
+      setFormData({
+        ...formData,
+        [fieldName]: value,
+      });
+    };
+    
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+    
+      // Create a FormData object to send the file along with other form data
+      const formDataToSend = new FormData();
+      formDataToSend.append("resume", resume);
+    
+      // Append other form fields to formDataToSend
+      for (const key in formData) {
+        if (formData.hasOwnProperty(key)) {
+          formDataToSend.append(key, formData[key]);
+        }
+      }
+    
+      axios
+        .post("http://localhost:4000/stuprofile", formDataToSend)
+        .then((result) => {
+          console.log("Student Profile Registration", result.data);
+          setInfoMessage("Successfully Updated");
+          setFormData(initialFormState);
+        })
+        .catch((err) => {
+          console.log(err);
+          setErrorMessage("Registration Failed");
+        });
+    };
+    
       
   const initialFields = [
-    { name: 'Name:',type:"text",  value: '' },
-    { name: 'Phone No: 0300-xxxxxxx',type:"text", value: '' },
-    { name: 'Address',type:"text", value: '' },
-    { name: 'Email:',type:"email", value: '' },
-    { name: 'Department:',type:"text", value: '' },
-    { name: 'Current Semester:',type:"number", value: '' },
-    { name: 'CGPA:',type:"number", value: '' },
-    { name: 'Skills:',type:"text", value: '' },
-    { name: 'Password:',type:"password", value: '' },
-    { name: 'Confirm Password:',type:"password", value: '' },
+    { name: 'Name',type:"text" },
+    { name: 'ContactNo',type:"text" },
+    { name: 'Address',type:"text" },
+    { name: 'Email',type:"email" },
+    { name: 'Department',type:"text" },
+    { name: 'CurrentSemester',type:"number" },
+    { name: 'CGPA',type:"number" },
+    { name: 'Skills',type:"text" },
+    // { name: 'Upload Resume',type:"file" },
+
     
     // Add more fields as needed
   ];
+  
+  // const [fields, setFields] = useState(initialFields);
 
-  const [fields, setFields] = useState(initialFields);
-
-  const handleFieldChange = (index, value) => {
-    const updatedFields = [...fields];
-    updatedFields[index].value = value;
-    setFields(updatedFields);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Process the form data here
-    console.log(fields);
-  };
 
     return (
       <div className="">
@@ -61,7 +149,7 @@ export default function StudentProfile() {
             } sm:block xl:w-[20%] lg:w-[30%] md:w-[35%] w-[100%] h-[100vh] bg-white sm:text-xl`}
           >
             <div className="text-center pt-8 pb-5">
-              <i class="fa-solid fa-user text-cyan-950 md:text-8xl text-4xl"></i>
+              <i className="fa-solid fa-user text-cyan-950 md:text-8xl text-4xl"></i>
             </div>
             <Menu />
           </div>
@@ -78,22 +166,23 @@ export default function StudentProfile() {
           <p className="sm:w-[70%] mb-6 max-sm:mx-10"> <span className="font-bold text-xl">Note:</span> Please ensure that you enter your information correctly during signup, as changes cannot be made afterward.</p>
         </div>
         <form onSubmit={handleSubmit}>
-            {fields.map((field, index) => (
-                <div key={field.name}>
+            {initialFields.map((field, index) => (
+                <div key={index}>
                 
                 <input
                     type={field.type}
-                    value={field.value}
+                    value={formData[field.name]}
                     placeholder={field.name}
                     className="2xl:w-[60%] xl:w-[55%] lg:w-[80%] py-2 sm:py-3 px-8 rounded sm:rounded-[10px] max-sm:mx-2 my-1 text-black"
-                    onChange={(e) => handleFieldChange(index, e.target.value)}
+                    onChange={(e) => handleFieldChange(field.name, e.target.value)}
                 />
                 </div>
             ))}
 
                 <div className="mt-3">
                                     <label htmlFor="Upload Resume" className=""> Upload Resume</label><br/>
-                                    <input type="file" className="my-2 " required/>
+                                    <input type="file" className="my-2 " required
+                                    onChange={(e) => setResume(e.target.files[0])}/>
             </div>
 
             <div className="my-3 text-white max-sm:mx-2">
@@ -107,6 +196,8 @@ export default function StudentProfile() {
             {/* back button */}
             <div className="mt-3 ml-1 max-sm:text-sm text-black text-semibold">
               <button><i className="fa-solid fa-arrow-left text-black sm:text-sm text-xs fon-semibold sm:mr-1  "></i> <Link to='/student-panel' className="font-semibold">Back to Dashboard</Link></button>
+              <h1 className="text-red-700 font-semibold mt-2">{errorMessage}</h1>
+              <h1 className="text-green-600 font-semibold mt-2">{infoMessage}</h1>
             </div>
           </div>
     </div>
@@ -117,3 +208,4 @@ export default function StudentProfile() {
       </div>
   );
 }
+
