ec1c6e52f9e06f4b0a215954d7db7ac264779d82
Ifra Shafique <86965873+IfraShafique@users.noreply.github.com>
2023-09-29 21:59:34 +0500

    Update UserDataReducer.js

--- src/Redux/Reducer/UserDataReducer.js
+++ src/Redux/Reducer/UserDataReducer.js
@@ -1,67 +1,56 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import jwtDecode from 'jwt-decode';
-
 
 export const fetchUserRegistrationData = createAsyncThunk(
-    "userData/ fetchUserRegistrationData",
-    async () => {
-        try {
-            const token = localStorage.getItem('jwt');
+  "userData/fetchUserRegistrationData",
+  async (_, { rejectWithValue }) => {
+    try {
+      const token = localStorage.getItem('jwt');
 
-            const decodedToken = jwtDecode(token);
-            if (token) {
-              const currentTime = Date.now() / 1000; // Convert to seconds
-              if (decodedToken.exp < currentTime) {
-                // Token has expired
-                // Perform logout or redirect to login page
-                localStorage.removeItem('jwt'); // Clear the expired token from local storage
-              }}
-        
-            console.log(decodedToken);
-            // Token exists, make authenticated request
-            axios.defaults.withCredentials = true;
-            // axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-            const response = await axios.get(
+      if (!token) {
+        return rejectWithValue('Token not found');
+      }
+
+      axios.defaults.withCredentials = true;
+
+      const response = await axios.get(
         "https://campus-recruitment-system-backend-btjmnyhxc-ifrashafique.vercel.app/userData",
-                headers: {
-                Authorization: `Bearer ${token}`
-  }
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
       );
 
       return response.data;
     } catch (error) {
-      throw error; // This will be captured by fetchUserRegistrationData.rejected action
+      throw error;
     }
+  }
+);
 
-    }
-)
-
-
-// Define a slice with a reducer
 const userDataSlice = createSlice({
-    name: 'userData',
-    initialState: {
-      data: null,
-      loading: false,
-      error: null,
-    },
-    reducers: {},
-    extraReducers: (builder) => {
-      builder
-        .addCase(fetchUserRegistrationData.pending, (state) => {
-          state.loading = true;
-        })
-        .addCase(fetchUserRegistrationData.fulfilled, (state, action) => {
-          state.loading = false;
-          state.data = action.payload;
-        })
-        .addCase(fetchUserRegistrationData.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.error.message; // Handle the error as needed
-    });
-    },
-  });
-  
-  // Export the reducer and actions
-  export default userDataSlice.reducer;
+  name: 'userData',
+  initialState: {
+    data: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUserRegistrationData.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchUserRegistrationData.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(fetchUserRegistrationData.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default userDataSlice.reducer;
