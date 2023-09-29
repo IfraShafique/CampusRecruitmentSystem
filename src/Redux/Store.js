1f09cb83b81a4886b182ac21d8c23501626b398a
Ifra Shafique <86965873+IfraShafique@users.noreply.github.com>
2023-09-29 22:00:48 +0500

    Update Store.js

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,14 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import RegistrationSlice from './Reducer/RegistrationSlice'; // Updated import
 import studentProfileSlice from './Reducer/studentProfileSlice';
 import postJobSlice from './Reducer/postJobSlice';
-import UserDataSlice from './Reducer/UserDataReducer';
+import userDataReducer from './Reducer/UserDataReducer';
 
 const store = configureStore({
   reducer: {
     studentRegistration: RegistrationSlice,
     studentProfile: studentProfileSlice,
     postJob: postJobSlice,
-    userData: UserDataSlice,
+    userData: userDataReducer,
   },
 });
 
